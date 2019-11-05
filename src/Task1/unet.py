65e25833173b921beef8db1efba4d965eb2c3393
shashankboosi <shashank1872@gmail.com>
2019-11-06 01:16:30 +1100

    [Model] Add the upsampling part for UNet

--- src/Task1/unet.py
+++ src/Task1/unet.py
@@ -6,10 +6,17 @@ import torch.nn.functional as F
 The dimensions of the H, W stays the same and it is done by using padding to the image 
 """
 
-
-def down_encode(in_channels, out_channels):
+def double_conv(in_channels, out_channels):
     return [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1),
             nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=1)]
+
+
+def up_conv(in_channels, out_channels):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=2, padding=1)
+
+
+def out_conv(in_channels, out_channels):
+    return nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1)
 
 
 class UNet(nn.Module):
@@ -17,17 +24,26 @@ class UNet(nn.Module):
     UNet Architecture
     """
 
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, num_classes):
         super().__init__()
         self.in_channels = in_channels
-        self.down_encode_conv_layer1 = down_encode(self.in_channels, 64)
-        self.down_encode_conv_layer2 = down_encode(64, 128)
-        self.down_encode_conv_layer3 = down_encode(128, 256)
-        self.down_encode_conv_layer4 = down_encode(256, 512)
-        self.down_encode_conv_layer5 = down_encode(512, 1024)
+        self.num_classes = num_classes
+        self.down_encode_conv_layer1 = double_conv(self.in_channels, 64)
+        self.down_encode_conv_layer2 = double_conv(64, 128)
+        self.down_encode_conv_layer3 = double_conv(128, 256)
+        self.down_encode_conv_layer4 = double_conv(256, 512)
+        self.down_encode_conv_layer5 = double_conv(512, 1024)
 
-        self.fc1 = nn.Linear(50 * 4 * 4, 256)
-        self.fc2 = nn.Linear(256, 10)
+        self.up_conv_layer1 = up_conv(1024, 512)
+        self.up_decode_conv_layer1 = double_conv(1024, 512)
+        self.up_conv_layer2 = up_conv(512, 256)
+        self.up_decode_conv_layer2 = double_conv(512, 256)
+        self.up_conv_layer3 = up_conv(256, 128)
+        self.up_decode_conv_layer3 = double_conv(256, 128)
+        self.up_conv_layer4 = up_conv(128, 64)
+        self.up_decode_conv_layer4 = double_conv(128, 64)
+
+        self.final_layer = out_conv(64, self.num_classes)
 
     def forward(self, x):
         # Down Layer 1
@@ -52,13 +68,38 @@ class UNet(nn.Module):
 
         # Down layer 5 (Global minimum of U of UNet architecture)
         x = F.relu(self.down_encode_conv_layer5[0](x))
-        layer5 = F.relu(self.down_encode_conv_layer5[1](x))
+        x = F.relu(self.down_encode_conv_layer5[1](x))
 
         # Up layer 1
-        x = F.upsample(layer5, scale_factor=2, mode='bilinear', align_corners=True)
+        x = F.upsample(x, scale_factor=2, mode='bilinear', align_corners=True)
+        # This will get the dimensions equal to the skip connection feature map dimensions
+        x = F.relu(self.up_conv_layer1(x))
+        x = torch.cat([x, layer4], dim=1)
+        x = F.relu(self.up_decode_conv_layer1[0](x))
+        x = F.relu(self.up_decode_conv_layer1[1](x))
 
+        # Up layer 2
+        x = F.upsample(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = F.relu(self.up_conv_layer2(x))
+        x = torch.cat([x, layer3], dim=1)
+        x = F.relu(self.up_decode_conv_layer2[0](x))
+        x = F.relu(self.up_decode_conv_layer2[1](x))
 
-        x = x.reshape(x.size(0), -1)
-        x = F.relu(self.fc1(x))
+        # Up layer 3
+        x = F.upsample(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = F.relu(self.up_conv_layer3(x))
+        x = torch.cat([x, layer2], dim=1)
+        x = F.relu(self.up_decode_conv_layer3[0](x))
+        x = F.relu(self.up_decode_conv_layer3[1](x))
+
+        # Up layer 4
+        x = F.upsample(x, scale_factor=2, mode='bilinear', align_corners=True)
+        x = F.relu(self.up_conv_layer4(x))
+        x = torch.cat([x, layer1], dim=1)
+        x = F.relu(self.up_decode_conv_layer4[0](x))
+        x = F.relu(self.up_decode_conv_layer4[1](x))
+
+        # Final layer
+        x = self.final_layer(x)
 
         return x
