7dec9173fff6f0734d008539e8f59b9a368fdc24
shashankboosi <shashank1872@gmail.com>
2019-11-06 00:25:33 +1100

    [Model] Add readme, requirements and transfer learning python file

--- src/Task1/utils.py
+++ src/Task1/utils.py
@@ -16,6 +16,9 @@ def save_image(file_name, image):
     cv2.imwrite(file_name + ".jpg", image)
 
 
+'''
+Example: print(len(read_images_from_folder("Train", "original_retinal_images", "jpg")))
+'''
 def read_images_from_folder(model_category, directory_name, extension_type):
     image_list = []
     dir_path = "../../resources/Task1/" + model_category + "/" + directory_name + "/*." + extension_type
@@ -26,5 +29,3 @@ def read_images_from_folder(model_category, directory_name, extension_type):
 
     return image_list
 
-
-print(len(read_images_from_folder("Train", "original_retinal_images", "jpg")))
