5aaf723dd299cc02bd6bd3a8230c90ef6a150d47
shashankboosi <shashank1872@gmail.com>
2019-11-11 02:18:13 +1100

    [Unet Train] Modify the dataset

--- src/Task1/dataset.py
+++ src/Task1/dataset.py
@@ -1,16 +1,16 @@
 import os
 from torch.utils.data import Dataset
-import torch
 import src.Task1.utils as utils
 import glob
-
+import numpy as np
 
 class IDRiDDataset(Dataset):
-    def __init__(self, data_root, image_extension_type='jpg'):
+    def __init__(self, data_root, image_extension_type='jpg', transform=None):
         self.data_root = data_root
         self.input = []
         self.output = []
         self.data_samples = []
+        self.transform = transform
         self.image_extension_type = image_extension_type
         self._init_dataset()
 
@@ -34,6 +34,9 @@ class IDRiDDataset(Dataset):
                 self.data_samples.append((self.input[i], self.output[i]))
         else:
             exit('Wrong storage of the augmented files, please check the image size of input and output')
+
+        if self.transform:
+            self.data_samples = self.transform(np.array(self.data_samples))
 
     def read_images_from_folder(self, dir_path):
         image_list = []
