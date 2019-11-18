29a54a815ef2f36b84e3a3f5e7ae6860b2924c15
shashankboosi <shashank1872@gmail.com>
2019-11-18 12:00:44 +1100

    [Metrics] Refine the dice loss

--- src/Task1/unet_train.py
+++ src/Task1/unet_train.py
@@ -32,20 +32,9 @@ class UNetTrain:
         Returns:
             dice_loss_v1: the Sørensen–Dice loss.
         """
-        num_classes = logits.shape[1]
-        if num_classes == 1:
-            true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            true_1_hot_f = true_1_hot[:, 0:1, :, :]
-            true_1_hot_s = true_1_hot[:, 1:2, :, :]
-            true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-            pos_prob = torch.sigmoid(logits)
-            neg_prob = 1 - pos_prob
-            probas = torch.cat([pos_prob, neg_prob], dim=1)
-        else:
-            true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
-            true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-            probas = F.softmax(logits, dim=1)
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
         true_1_hot = true_1_hot.type(logits.type())
         dims = (0,) + tuple(range(2, true.ndimension()))
         intersection = torch.sum(probas * true_1_hot, dims)
